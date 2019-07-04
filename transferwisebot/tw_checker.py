f6c3e1e9fb893d51623be22735247e5833be72ad
Alan Ostanik <alan.ostanik@farfetch.com>
2019-07-04 16:16:25 +0100

    Small Improvments

--- transferwisebot/tw_checker.py
+++ transferwisebot/tw_checker.py
@@ -12,24 +12,36 @@ def fetch_chat_id():
 def fetch_ideal_rate():
     return 0.23345
 
-def send_message_to(chat_id):
+def send_message_to(chat_id, savings):
     telegramKey = os.environ['BOT_KEY']
     url = "https://api.telegram.org/bot%s/sendMessage" % (telegramKey)
-    data = {"chat_id": chat_id, "text": "The BRL => EUR rate is really good, maybe you should transfer some money today :)"}
+
+    spacer = "\n---------------------------------------------------\n"
+    header = "**The BRL => EUR rate is really good**\n\n"
+    body = "You can earn more €%d since your last transfer" % (savings)
+    message = spacer+header+body+spacer
+
+    data = {"chat_id": chat_id, "text": message}
     requests.post(url, json=data)
 
-def fetch_tw_rate():
+def fetch_tw_object():
     url = "https://transferwise.com/gateway/v2/quotes/"
-    data = {"sourceAmount":1000,"sourceCurrency":"BRL","targetCurrency":"EUR","preferredPayIn":"BANK_TRANSFER","guaranteedTargetAmount":False}
-    return requests.post(url, json=data).json()['rate']
+    data = {"sourceAmount":5000,"sourceCurrency":"BRL","targetCurrency":"EUR","preferredPayIn":"BANK_TRANSFER","guaranteedTargetAmount":False}
+    return requests.post(url, json=data).json()
+
+def savings_from_last_transfer(tw_object):
+    value = tw_object['paymentOptions'][1]['targetAmount']
+    return value - 1138.07
 
 def rate_compare_bot():
     ideal_rate = fetch_ideal_rate()
     chat_id = fetch_chat_id()
-    actual_rate = fetch_tw_rate()
+    tw_object = fetch_tw_object()
+    actual_rate = tw_object['rate']
+    savings = savings_from_last_transfer(tw_object)
 
     if rate_compare(actual_rate, ideal_rate):
-        send_message_to(chat_id)
+        send_message_to(chat_id, savings)
 
 
 rate_compare_bot()
